9ef10dc547ee13084e4023e8153724dc267b6851
Randall <jr310@lab-rg07-06.cms.waikato.ac.nz>
2024-04-19 03:12:30 +1200

    first commit

---
+++ ByteToHex.java
@@ -0,0 +1,37 @@
+/**
+ * ByteToHex - Sourced from https://stackoverflow.com/questions/2817752/how-can-i-convert-a-byte-array-to-hexadecimal-in-java
+ */
+import java.io.File;
+import java.nio.file.Files;
+public class ByteToHex {
+  public static void main(String[] args) {
+    if (args.length == 1) {
+      try {
+        // Store the first argument as a string of the filename
+        String filename = args[0];
+
+        // Retrieve the text file specified by the user
+        File txtFile = new File(filename);
+
+        byte[] fileContent = Files.readAllBytes(txtFile.toPath());
+
+         // Convert bytes to hexadecimal string
+         StringBuilder hexString = new StringBuilder();
+         for (byte b : fileContent) {
+             String hex = Integer.toHexString(b & 0xFF);
+             if (hex.length() == 1) {
+                 hexString.append('0'); // Append 0 at front so hex is always two chars
+             }
+             hexString.append(hex);
+         }
+
+         // Print the hexadecimal string
+         System.out.println(hexString.toString());
+      } catch (Exception e) {
+        System.out.println("Error: " + e.toString());
+      }
+    } else {
+      System.out.println("Error, must include one argument: File name");
+    }
+  }
+}
---
+++ HexToByte.java
@@ -0,0 +1,33 @@
+
+/***
+ * https://www.geeksforgeeks.org/java-program-to-convert-hex-string-to-byte-array/
+ */
+
+import java.io.*;
+import java.util.*;
+import java.nio.file.Files;
+
+public class HexToByte {
+  public static void main(String[] args) {
+    try {
+      // Read from standard input
+      Scanner fileReader = new Scanner(System.in);
+
+      while (fileReader.hasNextLine()) {
+        String line = fileReader.nextLine();
+
+        // System.out.println(line.length());
+        // Parse each pair of characters in the line as hexadecimal
+        for (int i = 0; i < line.length(); i += 2) {
+          String hexByte = line.substring(i, i + 2);
+          int byteValue = Integer.parseInt(hexByte, 16);
+          System.out.write(byteValue);
+        }
+      }
+      System.out.flush();
+      fileReader.close();
+    } catch (Exception e) {
+      System.err.println("Error: " + e.toString());
+    }
+  }
+}
